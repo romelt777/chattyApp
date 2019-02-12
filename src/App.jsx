a49372a54c58a3b60eea61242a9527c752f32b70
Romel Tan <romel.tan777@gmail.com>
2019-02-12 16:54:04 +0000

    fixed  inital setup using message list and message components .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import Message from './Message.jsx';
-
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
   render() {
     return (
       <div>
-        <Message />
+        <MessageList />
         <ChatBar />
       </div>
     );
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
 
 class MessageList extends Component{
+  render(){
+    return (
+      <main className="messages">
+        <div className="message">
+          <Message />
+        </div>
+      </main>
 
-
-
-
+    );
+  }
 
 }
 
